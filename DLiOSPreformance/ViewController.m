22f1cbd754b1b9c0b8270a53dc126d901334aa1c
David <qiankunli@yahoo.com>
2018-11-11 16:11:54 +0800

    set up

--- DLiOSPreformance/ViewController.m
+++ DLiOSPreformance/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSArray *dataArray;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -16,8 +19,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _tableView.tableFooterView = [UIView new];
+    
+    self.dataArray = @[
+                       
+                       ];
 
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _dataArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *cellId = @"default_cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    cell.textLabel.text = @"cell";
+    return cell;
+}
+
+
 
 
 @end
